d2ee9bb9b08e91ef6485f7a1bfb3b16e4a88b4bc
Abbas Sadeghi <abbas.sadeghi1384@gmail.com>
2025-12-20 00:36:03 +0330

    feat(js): add persian translation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,27 @@
 async function handleClick() {
     const card = document.getElementById('main-card');
-    card.innerHTML = `<div class="w-8 h-8 border-4 border-primary border-t-secondary rounded-full animate-spin"></div>`;
 
-    axios.get('https://api.truthordarebot.xyz/api/dare')
-    .then(response => {
-        result = response.data.question;
-        card.textContent = response.data.question;
-    })
-    .catch(error => {
+    card.innerHTML = `<div class="w-8 h-8 border-4 border-primary border-t-secondary rounded-full animate-spin mx-auto"></div>`;
+    card.classList.add('text-center');
+
+    try {
+        card.classList.add('rtl', 'font-arabic', 'text-lg', 'font-medium');
+        const res = await axios.get('https://api.truthordarebot.xyz/api/dare');
+        const question = res.data.question;
+
+        const transres = await axios.get(`https://corsproxy.io/?url=https://ftapi.pythonanywhere.com/translate?sl=en&dl=fa&text=${question}`);
+        const translated = transres.data['destination-text'];
+
+        card.innerHTML = `
+        <div class="flex flex-col gap-2">
+        <p class="text-text font-display text-lg">${question}</p>
+        </br>
+        <p class="rtl font-arabic font-medium text-lg">${translated}</p>
+        </div>
+        `;
+
+    } catch (error) {
         console.error('error in dare function:', error);
-        card.textContent = error;
-    });
-    
+        card.textContent = "error";
+    }
 }
