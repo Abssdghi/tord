fcd8a35a5484e2dc790fff02718180405b2d1a8e
Abbas Sadeghi <abbas.sadeghi1384@gmail.com>
2025-12-19 23:56:15 +0330

    feat(js): add api call to button function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,15 @@
-function handleClick() {
-    document.getElementById('main-card').textContent = Math.round(Math.random()*100)+"%";
+async function handleClick() {
+    const card = document.getElementById('main-card');
+    card.innerHTML = `<div class="w-8 h-8 border-4 border-primary border-t-secondary rounded-full animate-spin"></div>`;
+
+    axios.get('https://api.truthordarebot.xyz/api/dare')
+    .then(response => {
+        result = response.data.question;
+        card.textContent = response.data.question;
+    })
+    .catch(error => {
+        console.error('error in dare function:', error);
+        card.textContent = error;
+    });
+    
 }
