3ea7146a394e15cf9bb4f166463b0ac41b58adbd
Abbas Sadeghi <abbas.sadeghi1384@gmail.com>
2025-12-23 21:59:29 +0330

    feat(js): add functions

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,26 @@
+async function go_loading(id='main-card') {
+    const card = document.getElementById(id);
+
+    card.innerHTML = `<div class="w-8 h-8 border-4 border-primary border-t-secondary rounded-full animate-spin mx-auto"></div>`;
+    card.classList.add('text-center');
+}
+
+
+async function translate(text,sl='en',dl='fa') {
+    const response = await axios.get(`https://corsproxy.io/?url=https://ftapi.pythonanywhere.com/translate?sl=${sl}&dl=${dl}&text=${text}`);
+    const translated = response.data['destination-text'];
+    return translated;    
+}
+
+
+async function req_to_baseurl(endpoint='dare') {
+    const res = await axios.get(`https://api.truthordarebot.xyz/api/${endpoint}`);
+    const question = res.data.question;
+    return question;
+    
+}
+
+
 async function handleClick() {
     const card = document.getElementById('main-card');
 
